d24460191635fef431f1c55a22b846573d5c90ef
Stamatis Mastromichalakis <stamatis@tmnetworks.gr>
2019-07-29 06:25:04 +0300

    minor complex numbers type improvement

--- frft2d.py
+++ frft2d.py
@@ -22,10 +22,10 @@ def frft2d(matrix, a = [0.7, 0.7]):
     data : numpy array
         The transformed 2D signal.
     """
-    temp = numpy.zeros((matrix.shape[0], matrix.shape[1]), dtype = 'complex')
+    temp = numpy.zeros((matrix.shape[0], matrix.shape[1]), dtype=numpy.complex)
     for k in range(0, matrix.shape[0]):
         temp[k, :] = frft(matrix[k, :] ,a[0])
-    out = numpy.zeros((temp.shape[0], temp.shape[1]), dtype = 'complex')
+    out = numpy.zeros((temp.shape[0], temp.shape[1]), dtype=numpy.complex)
     for m in range(0, temp.shape[1]):
         out[:, m] = frft(temp[:, m], a[1])
         
